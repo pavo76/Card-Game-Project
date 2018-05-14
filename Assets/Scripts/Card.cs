845f96275974e91ca2eaea8f4936e2b1b69ddc9d
pavo76 <dpavicic93@gmail.com>
2018-05-14 23:53:19 +0900

    Initial commit
    Created base structure of a board

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Card
+{
+    private int id;
+    private string name;
+    private int typeID;
+
+}
